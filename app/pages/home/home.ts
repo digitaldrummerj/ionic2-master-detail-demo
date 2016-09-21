3064c51cb5a6ad8842409811a61900f0c921a81d
Justin James <digitaldrummerj@gmail.com>
2016-09-20 21:01:05 -0700

    fixed link to details, updated home/about
    fixed issue with the bday date being a real date now instead of
    milliseconds since 1970.
    
    updated home page with details about the demo
    
    removed info from about and add left intentionally blank message.

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,16 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {ContactPage } from '../contact/contact'
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
   constructor(private navCtrl: NavController) {
-  
+
+  }
+
+  goToContacts() {
+    this.navCtrl.push(ContactPage);
   }
 }
